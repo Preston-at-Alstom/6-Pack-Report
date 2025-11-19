ba7fe5fcab8448164cf28a933661462da164e146
Preston-at-Alstom <preston.at.alstom@gmail.com>
2025-11-19 01:42:28 -0500

    Update crew_lineup.py

--- crew_lineup.py
+++ crew_lineup.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def to_df(file):
     
-    return pd.read_excel(file, sheet_name = 'Top', usecols= [0,2,3,4,5])
+    return pd.read_excel(file, sheet_name = 'Top', usecols= [0,2,3,4,5], dtype = {'Tour #' : 'Int32'})
+
 
 
